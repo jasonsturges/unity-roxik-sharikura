c30c66d804aa18f0f0f50bbae154addd54072f5b
Jason Sturges <jason@jasonsturges.com>
2019-10-05 14:12:02 -0500

    Sphere colors

--- Assets/Roxik.cs
+++ Assets/Roxik.cs
@@ -1,5 +1,6 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using UnityEngine;
+using Random = System.Random;
 
 public class Roxik : MonoBehaviour
 {
@@ -10,6 +11,17 @@ public class Roxik : MonoBehaviour
     {
         const float bet = 0.7f;
         const float offset = (8 - 1) * bet * 0.5f;
+        Color[] colors =
+        {
+            new Color(0.592f, 0.207f, 0.043f), 
+            new Color(0.149f, 0.431f, 0.647f), 
+            new Color(0.000f, 0.517f, 0.498f), 
+            new Color(0.184f, 0.505f, 0.556f),
+            new Color(0.031f, 0.568f, 0.486f),
+            new Color(0.419f, 0.270f, 0.549f),
+            new Color(0.478f, 0.270f, 0.149f)
+        };
+        Random random = new Random();
 
         for (var i = 0; i < 8; i++)
         {
@@ -24,9 +36,12 @@ public class Roxik : MonoBehaviour
                         y = j * bet - offset,
                         z = k * bet - offset
                     };
-                    sphere.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+                    sphere.transform.localScale = new Vector3(0.6f, 0.6f, 0.6f);
 
                     sphere.AddComponent<MotionProperties>();
+                    var sphereRenderer = sphere.GetComponent<Renderer>();
+                    sphereRenderer.material.SetColor("_Color", colors[random.Next(colors.Length)]);
+                    sphereRenderer.material.SetFloat("_Metallic", 0.3f);
 
                     Models.Add(sphere);
                 }
